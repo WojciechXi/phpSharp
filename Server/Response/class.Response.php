bd11449541b44e0d7d7022591d43942f22cccf78
WojciechXi <wojciech.ratuszynski@gmail.com>
2022-11-05 12:04:22 +0100

    Update

--- Server/Response/class.Response.php
+++ Server/Response/class.Response.php
@@ -18,7 +18,7 @@ namespace Server\Response {
 
         private mixed $content = null;
         private string $contentType = 'text/html';
-        private int $responseCode = 200;
+        private int $responseCode = 404;
 
         public function Header(string $key, string $value): self {
             header("{$key}: {$value}");
