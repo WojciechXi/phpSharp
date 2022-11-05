e5e8c8cf593ca6006aa083aea4d3b31c3434650c
WojciechXi <wojciech.ratuszynski@gmail.com>
2022-11-05 13:20:03 +0100

    Update

---
+++ ThirdParty/index.php
@@ -0,0 +1,2 @@
+<?php
+require_once __DIR__ . '/scssphp-1.11.0/scss.inc.php';
--- index.php
+++ index.php
@@ -4,3 +4,4 @@ require_once __DIR__ . '/class.Ob.php';
 require_once __DIR__ . '/System/index.php';
 require_once __DIR__ . '/Server/index.php';
 require_once __DIR__ . '/Program/index.php';
+require_once __DIR__ . '/ThirdParty/index.php';
