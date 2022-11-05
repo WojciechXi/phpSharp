d46dd03e8aede901dc0a4674ddbe9eed2916f879
WojciechXi <wojciech.ratuszynski@gmail.com>
2022-11-05 12:29:45 +0100

    Update

--- class.Ob.php
+++ class.Ob.php
@@ -2,14 +2,21 @@
 
 namespace Server {
 
+    use Exception;
+
     class Ob {
 
-        public static function Read(callable $action): string {
+        public static function Read(callable $action, array $replace = []): string {
             ob_start();
-            $action();
-            $content = ob_get_contents();
-            ob_end_flush();
-            return $content;
+            try {
+                $action();
+            } catch (Exception $exception) {
+                print_r($exception);
+            }
+            $contents = ob_get_contents();
+            ob_end_clean();
+            foreach ($replace as $key => $value) $contents = str_replace('{{' . $key . '}}', $value, $contents);
+            return $contents;
         }
     }
 }
