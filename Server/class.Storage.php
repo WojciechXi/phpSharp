a9772fad8ca8100a14fb170f9a419b8849815cbf
WojciechXi <wojciech.ratuszynski@gmail.com>
2022-11-07 20:10:35 +0100

    Update

--- Server/class.Storage.php
+++ Server/class.Storage.php
@@ -2,6 +2,8 @@
 
 namespace Server {
 
+    use Program\Program;
+
     class Storage {
 
         //Global
@@ -14,6 +16,34 @@ namespace Server {
         //Local
 
         private function __construct() {
+        }
+
+        public function GetUrl(string $item): string {
+            return Program::Instance()->GetUrl("Storage/{$item}");
+        }
+
+        public function GetPath(): string {
+            return Program::Instance()->GetPath('Storage');
+        }
+
+        public function Save(array $file): ?object {
+            $pathInfo = pathinfo($file['name']);
+
+            $fileName = $pathInfo['filename'];
+            $fileExtension = $pathInfo['extension'];
+
+            $fileAlias = md5(microtime() . $fileName .  $fileExtension);
+            $filePath = $this->GetPath() . "/{$fileAlias}.{$fileExtension}";
+
+            if (move_uploaded_file($file['tmp_name'], $filePath)) {
+                return (object)[
+                    'name' => $fileName,
+                    'extension' => $fileExtension,
+                    'alias' => $fileAlias,
+                ];
+            }
+
+            return null;
         }
     }
 }
