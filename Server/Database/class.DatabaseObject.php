4fbb4541b3274aa07a047adf2e40edf33b8247ff
WojciechXi <wojciech.ratuszynski@gmail.com>
2022-11-07 19:41:35 +0100

    Update

--- Server/Database/class.DatabaseObject.php
+++ Server/Database/class.DatabaseObject.php
@@ -42,6 +42,10 @@ namespace Server\Database {
             return static::Objects("ORDER BY dateOfUpdate DESC LIMIT {$limit}");
         }
 
+        public static function Random(int $limit = 9): array {
+            return static::Objects("ORDER BY RAND() LIMIT {$limit}");
+        }
+
         public static function ById(int $id): ?static {
             return static::Object("WHERE id = '{$id}'");
         }
