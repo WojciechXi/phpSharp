43f48409ea6ee8c1b96352e19e48577f315c44a7
WojciechXi <wojciech.ratuszynski@gmail.com>
2023-05-04 20:42:42 +0200

    Update

--- Server/View/class.View.php
+++ Server/View/class.View.php
@@ -23,23 +23,29 @@ namespace Server\View {
         //Local
 
         private function __construct() {
+            $this->params = [
+                'auth' => Auth::Instance(),
+                'config' => Config::Instance(),
+                'program' => Program::Instance(),
+                'request' => Request::Instance(),
+                'server' => Server::Instance(),
+                'session' => Session::Instance(),
+            ];
         }
+
+        private array $params = [];
 
         public function GetPath(string $view = null): string {
             return Program::Instance()->GetPath($view ? $view : 'Views');
         }
 
         public function Load(string $name, array $params = [], string $view = null): ?string {
+            $params = array_merge($this->params, $params);
+            $this->params = $params;
+
             $path = $this->GetPath($view) . '/' . $name . '.php';
 
             if (File::Exists($path)) {
-                $params['auth'] = Auth::Instance();
-                $params['config'] = Config::Instance();
-                $params['program'] = Program::Instance();
-                $params['request'] = Request::Instance();
-                $params['server'] = Server::Instance();
-                $params['session'] = Session::Instance();
-
                 $buffer = Ob::Read(function () use ($path, $params) {
                     extract($params);
                     require $path;
